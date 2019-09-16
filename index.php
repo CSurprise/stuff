4416b47d7e23bb281d605802f5f0ad5bd6574b85
Giovanni Morales <CSurprise@gmail.com>
2019-09-16 10:19:35 -0400

    Start of a website

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Giovanni Morales</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+		<link href="css/bootstrap.css" rel="stylesheet">
+    
+  </head>
+
+  <body>
+	   <?php
+     include('head.php');
+      ?>
+			
+			<script src="https://code.jquery.com/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+
+
+
+  </body>
+
+</html>
